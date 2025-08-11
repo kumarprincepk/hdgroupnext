ebc0618bbcf4b9e2839ea6ba10e0ffee472c1b1f
kumarprincepk <princekumar35071@gmail.com>
2025-08-11 07:54:53 +0530

    arranging files

---
+++ component/WhyTrustUsSection.jsx
@@ -0,0 +1,61 @@
+export default function WhyTrustUsSection() {
+  return (
+    <div className="tf-spacing-2">
+      <div className="tf-container">
+        <div className="row">
+          <div className="heading-section mb-30 text-left">
+            <h4 className="text-1 split-text effect-right">Why Trust Us</h4>
+          </div>
+          <div className="col-12">
+            <div className="wrap-counter style-1 tf-grid-layout-2  lg-col-4">
+              <div className="counter-item style-4 text-center ">
+                <div className="count">
+                  <div className="counter-number  mx-auto">
+                    <div className="odometer style-4 style-4-1">
+                      <strong>100</strong>
+                    </div>
+                    <span className="sub plus">+</span>
+                  </div>
+                  <p className="text-1 mt_-9 ">Dream house</p>
+                </div>
+              </div>
+              <div className="counter-item style-4 text-center">
+                <div className="count ">
+                  <div className="counter-number mx-auto ">
+                    <div className="odometer style-4 style-4-2">
+                      <strong>1</strong>
+                    </div>
+                    <span className="sub plus">+</span>
+                  </div>
+                  <p className="text-1 mt_-9">Happy clients</p>
+                </div>
+              </div>
+              <div className="counter-item style-4 text-center">
+                <div className="count">
+                  <div className="counter-number mx-auto ">
+                    <div className="odometer style-4 style-4-3">
+                      <strong>100</strong>
+                    </div>
+                    <span className="sub plus">+</span>
+                  </div>
+                  <p className="text-1 mt_-9 ">Local Agent</p>
+                </div>
+              </div>
+              <div className="counter-item style-4 text-center">
+                <div className="count ">
+                  <div className="counter-number mx-auto ">
+                    <div className="odometer style-4 style-4-4">
+                      <strong>1</strong>
+                    </div>
+                    <span className="sub plus">+</span>
+                  </div>
+                  <p className="text-1 mt_-9">Property Available</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
