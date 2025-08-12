c20281996a8a4efa80056b610cd45552546d2e12
kumarprincepk <princekumar35071@gmail.com>
2025-08-12 23:45:29 +0530

    fix style

--- component/TestimonialsSection.jsx
+++ component/TestimonialsSection.jsx
@@ -1,22 +1,53 @@
 "use client";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Autoplay  } from "swiper/modules";
+import { Navigation, Autoplay } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 export default function TestimonialsSection() {
+  const testimonials = [
+    {
+      id: 1,
+      text: "Phasellus efficitur dictum urna, non elementum mauris mollis a. Duis lacinia arcu ac lorem ultricies porta. Maecenas eleifend malesuada egestas. In eu eros hendrerit, pharetra justo id, blandit orci.",
+      name: "Ronald Richards",
+      role: "CEO Themesflat",
+    },
+    {
+      id: 2,
+      text: "From the moment I contacted them, I knew I was in good hands. The property exceeded all my expectations, and the team's expertise made the entire buying experience seamless.",
+      name: "Ronald Richards",
+      role: "CEO Themesflat",
+    },
+    {
+      id: 3,
+      text: "Phasellus efficitur dictum urna, non elementum mauris mollis a. Duis lacinia arcu ac lorem ultricies porta. Maecenas eleifend malesuada egestas. In eu eros hendrerit, pharetra justo id, blandit orci.",
+      name: "Ronald Richards",
+      role: "CEO Themesflat",
+    },
+    {
+      id: 4,
+      text: "From the moment I contacted them, I knew I was in good hands. The property exceeded all my expectations, and the team's expertise made the entire buying experience seamless.",
+      name: "Ronald Richards",
+      role: "CEO Themesflat",
+    },
+  ];
+
   return (
     <div className="section-testimonials tf-spacing-3">
       <div className="tf-container">
         <div className="flex flex-wrap">
           <div className="w-full">
+            {/* Heading */}
             <div className="heading-section mb-8 text-left">
-              <h4 className="text-1 split-text effect-right">Clients Testimonials</h4>
+              <h4 className="text-1 split-text effect-right">
+                Clients Testimonials
+              </h4>
             </div>
 
+            {/* Swiper Slider */}
             <Swiper
               modules={[Navigation, Autoplay]}
               slidesPerView={1}
@@ -25,93 +56,39 @@ export default function TestimonialsSection() {
               autoplay={{
                 delay: 2000,
                 disableOnInteraction: false,
+                pauseOnMouseEnter: true, 
               }}
               navigation={{
                 nextEl: ".nav-next-layout",
                 prevEl: ".nav-prev-layout",
               }}
               breakpoints={{
+                320: { slidesPerView: 1, spaceBetween: 20 },
                 768: { slidesPerView: 1, spaceBetween: 30 },
                 1024: { slidesPerView: 1, spaceBetween: 40 },
               }}
               className="sw-layout style-pagination"
+              style={{
+                WebkitOverflowScrolling: "touch",
+              }}
             >
-              {/* Slide 1 */}
-              <SwiperSlide>
-                <div className="wg-testimonial text-center">
-                  <p className="text-11 description">
-                    Phasellus efficitur dictum urna, non elementum mauris mollis a.
-                    Duis lacinia arcu ac lorem ultricies porta. Maecenas eleifend
-                    malesuada egestas. In eu eros hendrerit, pharetra justo id,
-                    blandit orci.
-                  </p>
-                  <div className="avatar"></div>
-                  <div className="author">
-                    <h6 className="name">
-                      <a href="#">Ronald Richards</a>
-                    </h6>
-                    <p className="text-2">CEO Themesflat</p>
+              {testimonials.map((item) => (
+                <SwiperSlide key={item.id}>
+                  <div className="wg-testimonial text-center">
+                    <p className="text-11 description">{item.text}</p>
+                    <div className="avatar"></div>
+                    <div className="author">
+                      <h6 className="name">
+                        <p>{item.name}</p>
+                      </h6>
+                      <p className="text-2">{item.role}</p>
+                    </div>
                   </div>
-                </div>
-              </SwiperSlide>
-
-              {/* Slide 2 */}
-              <SwiperSlide>
-                <div className="wg-testimonial text-center">
-                  <p className="text-11 description">
-                    From the moment I contacted them, I knew I was in good hands.
-                    The property exceeded all my expectations, and the team's
-                    expertise made the entire buying experience seamless.
-                  </p>
-                  <div className="avatar"></div>
-                  <div className="author">
-                    <h6 className="name">
-                      <a href="#">Ronald Richards</a>
-                    </h6>
-                    <p className="text-2">CEO Themesflat</p>
-                  </div>
-                </div>
-              </SwiperSlide>
-
-              {/* Slide 3 */}
-              <SwiperSlide>
-                <div className="wg-testimonial text-center">
-                  <p className="text-11 description">
-                    Phasellus efficitur dictum urna, non elementum mauris mollis a.
-                    Duis lacinia arcu ac lorem ultricies porta. Maecenas eleifend
-                    malesuada egestas. In eu eros hendrerit, pharetra justo id,
-                    blandit orci.
-                  </p>
-                  <div className="avatar"></div>
-                  <div className="author">
-                    <h6 className="name">
-                      <a href="#">Ronald Richards</a>
-                    </h6>
-                    <p className="text-2">CEO Themesflat</p>
-                  </div>
-                </div>
-              </SwiperSlide>
-
-              {/* Slide 4 */}
-              <SwiperSlide>
-                <div className="wg-testimonial text-center">
-                  <p className="text-11 description">
-                    From the moment I contacted them, I knew I was in good hands.
-                    The property exceeded all my expectations, and the team's
-                    expertise made the entire buying experience seamless.
-                  </p>
-                  <div className="avatar"></div>
-                  <div className="author">
-                    <h6 className="name">
-                      <a href="#">Ronald Richards</a>
-                    </h6>
-                    <p className="text-2">CEO Themesflat</p>
-                  </div>
-                </div>
-              </SwiperSlide>
+                </SwiperSlide>
+              ))}
             </Swiper>
 
-            {/* Swiper Navigation Buttons */}
+            {/* Navigation Buttons */}
             <div className="sw-wrap-btn">
               <div className="swiper-button-prev sw-button nav-prev-layout">
                 <i className="icon-arrow-left-3"></i>
@@ -120,7 +97,6 @@ export default function TestimonialsSection() {
                 <i className="icon-arrow-right-3"></i>
               </div>
             </div>
-
           </div>
         </div>
       </div>
--- component/WhatWeDoSection.jsx
+++ component/WhatWeDoSection.jsx
@@ -1,6 +1,45 @@
+"use client";
+
 import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import Link from "next/link";
 
 export default function WhatWeDoSection() {
+  const services = [
+    {
+      id: 1,
+      title: "New Construction",
+      description:
+        "We design spaces with a timeless vision, balancing innovation and aesthetic appeal.",
+      image: "/images/Screenshot.png",
+    },
+    {
+      id: 2,
+      title: "Real Estate Consultancy",
+      description:
+        "We blend our 30 years of expertise with our dedication to service to give our clients the best.",
+      image: "/images/Screenshot (85) (1).png",
+    },
+    {
+      id: 3,
+      title: "Interior Design",
+      description:
+        "Our interior design solutions are client-centric, mixing bold elements with timeless sophistication.",
+      image: "/images/agent-item.jpg",
+    },
+    {
+      id: 4,
+      title: "Maintenance Services",
+      description:
+        "Our comprehensive maintenance solutions ensure that your space remains efficient.",
+      image: "/images/section-pre-approved1.jpg",
+    },
+  ];
+
   return (
     <section className="section-local-agent">
       <div className="tf-container">
@@ -9,163 +48,57 @@ export default function WhatWeDoSection() {
             <div className="heading-section mb-7 text-left">
               <h4 className="text-1 split-text effect-right">What we do</h4>
             </div>
-            <div
-              className="swiper style-pagination tf-sw-mobile-1"
-              data-screen="575"
-              data-preview="1"
-              data-space="15"
+
+            <Swiper
+              modules={[Pagination, Autoplay]}
+              pagination={{ clickable: true }}
+              loop={true}
+              grabCursor={true}
+              autoplay={{
+                delay: 2000,
+                disableOnInteraction: false,
+              }}
+              spaceBetween={15}
+              breakpoints={{
+                320: { slidesPerView: 1, spaceBetween: 15 },
+                575: { slidesPerView: 1, spaceBetween: 15 },
+                768: { slidesPerView: 2, spaceBetween: 20 },
+                1024: { slidesPerView: 4, spaceBetween: 30 },
+              }}
+              style={{
+                WebkitOverflowScrolling: "touch", // smooth on iOS Safari
+              }}
             >
-              <div className="swiper-wrapper tf-layout-mobile-sm sm-col-2 lg-col-4 mb-12">
-                {/* Slide 1 */}
-                <div className="swiper-slide">
-                  <div
-                    className="agent-item hover-img style-2 wow animate__fadeInUp animate__animated"
-                    data-wow-duration="1s"
-                    data-wow-delay="0s"
-                  >
+              {services.map((service) => (
+                <SwiperSlide key={service.id}>
+                  <div className="agent-item hover-img style-2">
                     <div className="image-wrap">
-                      <a href="#">
+                      <Link href="/">
                         <Image
+                          src={service.image}
+                          alt={service.title}
+                          width={200}
+                          height={200}
                           className="lazyload"
-                          data-src="/images/Screenshot.png"
-                          src="/images/Screenshot.png"
-                          alt=""
-                          width={50}
-                          height={50}
                         />
-                      </a>
+                      </Link>
                     </div>
                     <div className="content justify-center">
                       <div className="author text-center">
                         <h4 className="name lh-30">
-                          <a>New Construction</a>
+                          <Link href="/">{service.title}</Link>
                         </h4>
-                        <p className="text-3 lh-18">
-                          we design spaces with a timeless vision, balancing
-                          innovation and aesthetic appeal
-                        </p>
-                        <a href="#" className="tf-btn-link">
+                        <p className="text-3 lh-18">{service.description}</p>
+                        <Link href="/" className="tf-btn-link">
                           <span>Read More</span>{" "}
                           <i className="icon-circle-arrow"></i>
-                        </a>
+                        </Link>
                       </div>
                     </div>
                   </div>
-                </div>
-
-                {/* Slide 2 */}
-                <div className="swiper-slide">
-                  <div
-                    className="agent-item hover-img style-2 wow animate__fadeInUp animate__animated"
-                    data-wow-duration="1s"
-                    data-wow-delay="0.1s"
-                  >
-                    <div className="image-wrap">
-                      <a href="#">
-                        <Image
-                          className="lazyload"
-                          data-src="/images/Screenshot (85) (1).png"
-                          src="/images/Screenshot (85) (1).png"
-                          alt=""
-                          width={50}
-                          height={50}
-                        />
-                      </a>
-                    </div>
-                    <div className="content justify-center">
-                      <div className="author text-center">
-                        <h4 className="name lh-30">
-                          <a>Real Estate Consultancy</a>
-                        </h4>
-                        <p className="text-3 lh-18">
-                          We blend our 30 years of expertise with our dedication
-                          to service to give our clients the best
-                        </p>
-                        <a href="#" className="tf-btn-link">
-                          <span>Read More</span>{" "}
-                          <i className="icon-circle-arrow"></i>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                {/* Slide 3 */}
-                <div className="swiper-slide">
-                  <div
-                    className="agent-item hover-img style-2 wow animate__fadeInUp animate__animated"
-                    data-wow-duration="1s"
-                    data-wow-delay="0.2s"
-                  >
-                    <div className="image-wrap">
-                      <a href="#">
-                        <Image
-                          className="lazyload"
-                          data-src="/images/agent-item.jpg"
-                          src="/images/agent-item.jpg"
-                          alt=""
-                          width={50}
-                          height={50}
-                        />
-                      </a>
-                    </div>
-                    <div className="content justify-center">
-                      <div className="author text-center">
-                        <h4 className="name lh-30">
-                          <a>Interior Design</a>
-                        </h4>
-                        <p className="text-3 lh-18">
-                          Our interior design solutions are client-centric,
-                          mixing bold elements with timeless sophistication.
-                        </p>
-                        <a href="#" className="tf-btn-link">
-                          <span>Read More</span>{" "}
-                          <i className="icon-circle-arrow"></i>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                {/* Slide 4 */}
-                <div className="swiper-slide">
-                  <div
-                    className="agent-item hover-img style-2 wow animate__fadeInUp animate__animated"
-                    data-wow-duration="1s"
-                    data-wow-delay="0.3s"
-                  >
-                    <div className="image-wrap">
-                      <a>
-                        <Image
-                          className="lazyload"
-                          data-src="/images/section-pre-approved1.jpg"
-                          src="/images/section-pre-approved1.jpg"
-                          alt=""
-                          width={50}
-                          height={50}
-                        />
-                      </a>
-                    </div>
-                    <div className="content justify-center">
-                      <div className="author text-center">
-                        <h4 className="name lh-30">
-                          <a>Maintenance Services</a>
-                        </h4>
-                        <p className="text-3 lh-18">
-                          Our comprehensive maintenance solutions ensure that
-                          your space remains efficient,
-                        </p>
-                        <a href="#" className="tf-btn-link">
-                          <span>Read More</span>{" "}
-                          <i className="icon-circle-arrow"></i>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="sw-pagination sw-pagination-mb-1 text-center lg:hidden block mb-5"></div>
-            </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </div>
         </div>
       </div>
