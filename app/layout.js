f21253345243ade51d512ff7ae2427c729b14f4e
Prince <prince.kumar@velocis.co.in>
2025-08-14 15:28:58 +0530

    add some packages for production css

--- app/layout.js
+++ app/layout.js
@@ -4,11 +4,13 @@ import "../style/globals.css";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  display: "swap",
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  display: 'swap',
 });
 
 export const metadata = {
