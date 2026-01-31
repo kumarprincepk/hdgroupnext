de95b4dbb6d4ba4de77894da501804ec639f6185
kumarprincepk <princekumar35071@gmail.com>
2026-01-31 15:22:04 +0530

    route update in header

--- components/headerSection/Header.jsx
+++ components/headerSection/Header.jsx
@@ -1,15 +1,12 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { usePathname } from "next/navigation";
+import { usePathname,useRouter } from "next/navigation";
 import serviceDetails from "@/public/data/pageInfo";
 import Link from "next/link";
 import Image from "next/image";
 import projectInfo from "@/public/data/projectInfo";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useRouter } from "next/navigation";
-
-
 
 export default function Header() {
  
@@ -17,7 +14,6 @@ export default function Header() {
   const [isSticky, setIsSticky] = useState(false);
   const pathname = usePathname();
   const router = useRouter();
-
 
   useEffect(() => {
     if (pathname !== "/") return;
@@ -28,7 +24,6 @@ export default function Header() {
       } else {
         setIsSticky(true);
       }
-
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -37,34 +32,37 @@ export default function Header() {
     };
   }, [pathname]);
 
-
-  useEffect(() => {
-    // Select all links that should dismiss the modal
-    const links = document.querySelectorAll('#staticBackdrop [data-bs-dismiss="modal"][href]');
-
-    const handleClick = (e) => {
-      e.preventDefault();
-      const href = e.currentTarget.getAttribute('href');
-
-      // Close the modal manually
-      const modalEl = document.getElementById('staticBackdrop');
-      if (modalEl) {
-        const modalInstance = bootstrap.Modal.getInstance(modalEl);
-        modalInstance?.hide();
+  // Function to close the Bootstrap modal
+  const closeModal = () => {
+    const modal = document.getElementById('staticBackdrop');
+    if (modal) {
+      // Use Bootstrap's modal instance to close it properly
+      const modalInstance = window.bootstrap?.Modal.getInstance(modal);
+      if (modalInstance) {
+        modalInstance.hide();
+      } else {
+        // Fallback if Bootstrap not loaded yet
+        modal.classList.remove('show');
+        modal.style.display = 'none';
+        document.body.classList.remove('modal-open');
+        const backdrop = document.querySelector('.modal-backdrop');
+        if (backdrop) {
+          backdrop.remove();
+        }
       }
+    }
+  };
 
-      // Delay navigation until modal close animation completes
-      setTimeout(() => {
-        router.push(href);
-      }, 300); // Adjust delay if your modal animation is longer
-    };
-
-    links.forEach((link) => link.addEventListener('click', handleClick));
-
-    return () => {
-      links.forEach((link) => link.removeEventListener('click', handleClick));
-    };
-  }, [router]);
+  // Handler for mobile menu links
+  const handleMobileLinkClick = (e, href) => {
+    e.preventDefault();
+    closeModal();
+    
+    // Small delay to allow modal animation to complete
+    setTimeout(() => {
+      router.push(href);
+    }, 300);
+  };
 
   const isHome = pathname === "/";
   return (
@@ -260,115 +258,122 @@ export default function Header() {
         </nav>
       </header>
 
-       <div
-      className="modal fade left"
-      id="staticBackdrop"
-      data-bs-backdrop="static"
-      data-bs-keyboard="false"
-      aria-labelledby="staticBackdropLabel"
-      aria-hidden="true"
-    >
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h1 className="modal-title fs-5" id="staticBackdropLabel">
-              <img
-                src="/images/logo/logo-dark.png"
-                className="img-fluid"
-                height={200}
-                width={200}
-                alt="logo"
-              />
-            </h1>
-            <button
-              type="button"
-              className="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
+      <div
+        className="modal fade left"
+        id="staticBackdrop"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="staticBackdropLabel">
+                <img
+                  src="/images/logo/logo-dark.png"
+                  className="img-fluid"
+                  height={200}
+                  width={200}
+                  alt="logo"
+                />
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
 
-          <div className="modal-body">
-            <nav className="navbar navbar-expand-lg mobile-menu">
-              <div className="container-fluid">
-                <div className="navbar-collapse collapse show" id="navbarNav">
-                  <div className="ms-auto w-100 d-flex justify-content-end menu-on-hover">
-                    <ul className="navbar-nav navigation">
-                      <li className="current-menu">
-                        <Link href="/" data-bs-dismiss="modal" aria-label="Close">
-                          Home
-                        </Link>
-                      </li>
-                      <li>
-                        <Link href="/about" data-bs-dismiss="modal" aria-label="Close">
-                          About
-                        </Link>
-                      </li>
+            <div className="modal-body">
+              <nav className="navbar navbar-expand-lg mobile-menu">
+                <div className="container-fluid">
+                  <div className="navbar-collapse collapse show" id="navbarNav">
+                    <div className="ms-auto w-100 d-flex justify-content-end menu-on-hover">
+                      <ul className="navbar-nav navigation">
+                        <li className="current-menu">
+                          <Link 
+                            href="/" 
+                            onClick={(e) => handleMobileLinkClick(e, '/')}
+                          >
+                            Home
+                          </Link>
+                        </li>
+                        <li>
+                          <Link 
+                            href="/about" 
+                            onClick={(e) => handleMobileLinkClick(e, '/about')}
+                          >
+                            About
+                          </Link>
+                        </li>
 
-                      <li className="has-child">
-                        <Link
-                          href="#"
-                          role="button"
-                          data-bs-toggle="dropdown"
-                          aria-expanded="false"
-                        >
-                          Projects
-                        </Link>
-                        <ul className="dropdown-menu submenu">
-                          {projectInfo.map((project, index) => (
-                            <li key={index}>
-                              <Link
-                                className="dropdown-item"
-                                href={`/projects/${project?.slug}`}
-                                data-bs-dismiss="modal"
-                                aria-label="Close"
-                              >
-                                {project.heading}
-                              </Link>
-                            </li>
-                          ))}
-                        </ul>
-                      </li>
+                        <li className="has-child">
+                          <Link
+                            href="#"
+                            role="button"
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false"
+                          >
+                            Projects
+                          </Link>
+                          <ul className="dropdown-menu submenu">
+                            {projectInfo.map((project, index) => (
+                              <li key={index}>
+                                <Link
+                                  className="dropdown-item"
+                                  href={`/projects/${project?.slug}`}
+                                  onClick={(e) => handleMobileLinkClick(e, `/projects/${project?.slug}`)}
+                                >
+                                  {project.heading}
+                                </Link>
+                              </li>
+                            ))}
+                          </ul>
+                        </li>
 
-                      <li className="has-child">
-                        <Link
-                          href="#"
-                          role="button"
-                          data-bs-toggle="dropdown"
-                          aria-expanded="false"
-                        >
-                          Services
-                        </Link>
-                        <ul className="dropdown-menu submenu">
-                          {serviceDetails.map((service) => (
-                            <li key={service.id}>
-                              <Link
-                                className="dropdown-item"
-                                href={`/services/${service.slug}`}
-                                data-bs-dismiss="modal"
-                                aria-label="Close"
-                              >
-                                {service.heading}
-                              </Link>
-                            </li>
-                          ))}
-                        </ul>
-                      </li>
+                        <li className="has-child">
+                          <Link
+                            href="#"
+                            role="button"
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false"
+                          >
+                            Services
+                          </Link>
+                          <ul className="dropdown-menu submenu">
+                            {serviceDetails.map((service) => (
+                              <li key={service.id}>
+                                <Link
+                                  className="dropdown-item"
+                                  href={`/services/${service.slug}`}
+                                  onClick={(e) => handleMobileLinkClick(e, `/services/${service.slug}`)}
+                                >
+                                  {service.heading}
+                                </Link>
+                              </li>
+                            ))}
+                          </ul>
+                        </li>
 
-                      <li>
-                        <Link href="/contact" data-bs-dismiss="modal" aria-label="Close">
-                          Contact
-                        </Link>
-                      </li>
-                    </ul>
+                        <li>
+                          <Link 
+                            href="/contact" 
+                            onClick={(e) => handleMobileLinkClick(e, '/contact')}
+                          >
+                            Contact
+                          </Link>
+                        </li>
+                      </ul>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </nav>
+              </nav>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
